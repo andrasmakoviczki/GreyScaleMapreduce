d2289aafbe1e1028dec734f9f19b5ae3da073734
Andras Makoviczki <makoviczki.andras@gmail.com>
2018-05-08 12:08:26 +0200

    Set key

--- src/main/java/GreyScaleMapreduce.java
+++ src/main/java/GreyScaleMapreduce.java
@@ -1,9 +1,11 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.*;
+
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.awt.image.BufferedImage;
@@ -23,8 +25,10 @@ public class GreyScaleMapreduce {
         //job.setCombinerClass();
         //job.setReducerClass();
 
-        job.setOutputKeyClass(LongWritable.class);
-        job.setOutputValueClass(Text.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(BytesWritable.class);
+
+        job.setInputFormatClass(SequenceFileInputFormat.class);
 
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job,new Path(args[1]));
@@ -32,9 +36,9 @@ public class GreyScaleMapreduce {
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 
-    public static class ImgGreyMapper extends Mapper<LongWritable,Text,LongWritable,Text>{
+    public static class ImgGreyMapper extends Mapper<Text,BytesWritable,Text,BytesWritable>{
 
-        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        protected void map(Text key, BytesWritable value, Context context) throws IOException, InterruptedException {
             /*SequenceFile.Reader reader = new SequenceFile.Reader(conf,
                     SequenceFile.Reader.file(outputPath));
 
